6664a1ca33710a88c8d461c00ee0bd46686dea23
madiyar99 <madiyar021@gmail.com>
2022-10-05 23:44:15 +0300

    Lab1 v1.0

---
+++ ClassLibrary/ClassLibrary/ClassArcFace.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using Microsoft.ML.OnnxRuntime;
+using Microsoft.ML.OnnxRuntime.Tensors;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using System.Threading.Tasks.Dataflow;
+
+namespace ClassLibrary;
+public class ClassArcFace
+{
+    private InferenceSession session;
+
+    public ClassArcFace()
+    {
+        using var modelStream = typeof(ClassArcFace).Assembly.GetManifestResourceStream("ClassLibrary.arcfaceresnet100-8.onnx");
+        using var memoryStream = new MemoryStream();
+        modelStream.CopyTo(memoryStream);
+        var sessionOptions = new SessionOptions();
+        sessionOptions.ExecutionMode = ExecutionMode.ORT_PARALLEL;
+        this.session = new InferenceSession(memoryStream.ToArray(), sessionOptions);
+    }
+
+    ~ClassArcFace()
+    {
+        this.session.Dispose();
+    }
+
+    public async Task<Tuple<float, float>> startCalculations(Image<Rgb24> face1, Image<Rgb24> face2, CancellationToken token)
+    {
+        var buffer = new BufferBlock<Image<Rgb24>>();
+
+        var t1 = new TransformBlock<Image<Rgb24>, float[]>(face =>
+        {
+            if (token.IsCancellationRequested)
+                token.ThrowIfCancellationRequested(); // генерируем исключение
+
+            var inputs = new List<NamedOnnxValue> { NamedOnnxValue.CreateFromTensor("data", ImageToTensor(face)) };
+            using IDisposableReadOnlyCollection<DisposableNamedOnnxValue> results = session.Run(inputs);
+
+            if (token.IsCancellationRequested)
+                token.ThrowIfCancellationRequested(); // генерируем исключение
+
+            return Normalize(results.First(v => v.Name == "fc1").AsEnumerable<float>().ToArray());
+        }, new ExecutionDataflowBlockOptions
+        {
+            CancellationToken = token
+        });
+
+        buffer.Post(face1);
+        buffer.Post(face2);
+
+        buffer.LinkTo(t1);
+        buffer.Complete();
+
+        float[] embeddings1 = await t1.ReceiveAsync();
+        float[] embeddings2 = await t1.ReceiveAsync();
+
+        var dist = Distance(embeddings1, embeddings2);
+        var similarity = Similarity(embeddings1, embeddings2);
+        await dist;
+        await similarity;
+
+        return Tuple.Create(dist.Result, similarity.Result);
+
+    }
+
+    async Task<float> Distance(float[] v1, float[] v2)
+    {
+        return await Task<float>.Factory.StartNew(() => {
+            return Length(v1.Zip(v2).Select(p => p.First - p.Second).ToArray());
+        });
+    }
+
+    async Task<float> Similarity(float[] v1, float[] v2)
+    {
+        return await Task<float>.Factory.StartNew(() =>
+        {
+            return v1.Zip(v2).Select(p => p.First * p.Second).Sum();
+        });
+    }
+
+    float Length(float[] v) => (float)Math.Sqrt(v.Select(x => x * x).Sum());
+
+    float[] Normalize(float[] v)
+    {
+        var len = Length(v);
+        return v.Select(x => x / len).ToArray();
+    }
+
+    DenseTensor<float> ImageToTensor(Image<Rgb24> img)
+    {
+        var w = img.Width;
+        var h = img.Height;
+        var t = new DenseTensor<float>(new[] { 1, 3, h, w });
+
+        img.ProcessPixelRows(pa =>
+        {
+            for (int y = 0; y < h; y++)
+            {
+                Span<Rgb24> pixelSpan = pa.GetRowSpan(y);
+                for (int x = 0; x < w; x++)
+                {
+                    t[0, 0, y, x] = pixelSpan[x].R;
+                    t[0, 1, y, x] = pixelSpan[x].G;
+                    t[0, 2, y, x] = pixelSpan[x].B;
+                }
+            }
+        });
+        return t;
+    }
+
+    public void Dispose()
+    {
+        throw new NotImplementedException();
+    }
+
+    /*
+    float[] GetEmbeddings(Image<Rgb24> face)
+    {
+        var inputs = new List<NamedOnnxValue> { NamedOnnxValue.CreateFromTensor("data", ImageToTensor(face)) };
+        using IDisposableReadOnlyCollection<DisposableNamedOnnxValue> results = session.Run(inputs);
+        return Normalize(results.First(v => v.Name == "fc1").AsEnumerable<float>().ToArray());
+    }
+    */
+
+}
+
---
+++ ClassLibrary/testConsole/Program.cs
@@ -0,0 +1,107 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics;
+using Microsoft.ML.OnnxRuntime;
+using System.Linq;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using System.Threading;
+using System.Threading.Tasks;
+
+using ClassLibrary;
+
+namespace testConsole 
+{
+    internal class Program
+    {
+
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+            /*
+            Console.WriteLine("Predicting contents of image...");
+            
+            foreach (var kv in obj1.session.InputMetadata)
+                Console.WriteLine($"{kv.Key}: {MetadataToString(kv.Value)}");
+            foreach (var kv in obj1.session.OutputMetadata)
+                Console.WriteLine($"{kv.Key}: {MetadataToString(kv.Value)}]");
+
+            string MetadataToString(NodeMetadata metadata)
+            => $"{metadata.ElementType}[{String.Join(",", metadata.Dimensions.Select(i => i.ToString()))}]";
+            */
+
+            ClassArcFace obj1 = new ClassArcFace();
+            ClassArcFace obj2 = new ClassArcFace();
+
+            CancellationTokenSource cancelTokenSource = new CancellationTokenSource();
+            CancellationToken token = cancelTokenSource.Token;
+
+            using var face1 = Image.Load<Rgb24>("face1.png");
+            using var face2 = Image.Load<Rgb24>("face2.png");
+
+            using var face3 = Image.Load<Rgb24>("madiyar1.jpg");
+            using var face4 = Image.Load<Rgb24>("madiyar2.jpg");
+
+            //синхронный запуск
+            Stopwatch sw1 = new Stopwatch();
+            sw1.Start();
+            var res1 = obj1.startCalculations(face1, face2, token);
+            await res1;
+            var res2 = obj1.startCalculations(face1, face2, token);
+            await res2;
+            var res3 = obj1.startCalculations(face1, face2, token);
+            await res3;
+            sw1.Stop();
+
+            Console.WriteLine("synchronous calculations:");
+            Console.WriteLine("------ Results 1 ------");
+            Console.WriteLine($"Distance = {res1.Result.Item1}");
+            Console.WriteLine($"Similarity =  {res1.Result.Item2}");
+            Console.WriteLine("------ Results 2 ------");
+            Console.WriteLine($"Distance = {res2.Result.Item1}");
+            Console.WriteLine($"Similarity =  {res2.Result.Item2}");
+            Console.WriteLine("------ Results 3 ------");
+            Console.WriteLine($"Distance = {res3.Result.Item1}");
+            Console.WriteLine($"Similarity =  {res3.Result.Item2}");
+            Console.WriteLine("Time elapsed: {0} ms", sw1.ElapsedMilliseconds);
+            Console.WriteLine("------------------------------");
+
+
+            //асинхронный запуск
+            Stopwatch sw2 = new Stopwatch();
+            sw2.Start();
+            var res4 = obj2.startCalculations(face1, face2, token);
+            var res5 = obj2.startCalculations(face1, face2, token);
+            var res6 = obj2.startCalculations(face1, face2, token);
+            await res4;
+            await res5;
+            await res6;
+            sw2.Stop();
+
+            Console.WriteLine("asynchronous calculations:");
+            Console.WriteLine("------ Results 1 ------");
+            Console.WriteLine($"Distance = {res4.Result.Item1}");
+            Console.WriteLine($"Similarity =  {res4.Result.Item2}");
+            Console.WriteLine("------ Results 2 ------");
+            Console.WriteLine($"Distance = {res5.Result.Item1}");
+            Console.WriteLine($"Similarity =  {res5.Result.Item2}");
+            Console.WriteLine("------ Results 3 ------");
+            Console.WriteLine($"Distance = {res6.Result.Item1}");
+            Console.WriteLine($"Similarity =  {res6.Result.Item2}");
+            Console.WriteLine("Time elapsed: {0} ms", sw2.ElapsedMilliseconds);
+            Console.WriteLine("------------------------------");
+
+            //Отмена вычислений
+
+            var res7 = obj2.startCalculations(face1, face2, token);
+            //Thread.Sleep(1000);
+            cancelTokenSource.Cancel();
+            Console.WriteLine("------ Results 1 ------");
+            Console.WriteLine($"Distance = {res7.Result.Item1}");
+            Console.WriteLine($"Similarity =  {res7.Result.Item2}");
+            cancelTokenSource.Dispose();
+        }
+    }
+}
