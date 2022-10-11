4fd9cc6db4e131514960e167c8bbb7dcbc94389d
madiyar99 <madiyar021@gmail.com>
2022-10-11 14:52:41 +0300

    Lab1 v3.0

--- ClassLibrary/ClassLibrary/ClassArcFace.cs
+++ ClassLibrary/ClassLibrary/ClassArcFace.cs
@@ -11,25 +11,20 @@ using System.Threading.Tasks.Dataflow;
 using System.Collections.Concurrent;
 
 namespace ClassLibrary;
-public class ClassArcFace
+public class ClassArcFace : IDisposable
 {
     private InferenceSession session;
-    private TransformBlock<Image<Rgb24>, float[]> transform;
-    private BufferBlock<Image<Rgb24>> buffer;
     private ConcurrentDictionary<Image<Rgb24>, float[]> embeddingsDict;
 
-    public ClassArcFace(CancellationToken token)
+    public ClassArcFace()
     {
         using var modelStream = typeof(ClassArcFace).Assembly.GetManifestResourceStream("ClassLibrary.arcfaceresnet100-8.onnx");
         using var memoryStream = new MemoryStream();
         modelStream.CopyTo(memoryStream);
         var sessionOptions = new SessionOptions();
         sessionOptions.ExecutionMode = ExecutionMode.ORT_PARALLEL;
-        this.session = new InferenceSession(memoryStream.ToArray(), sessionOptions);
-        this.transform = new TransformBlock<Image<Rgb24>, float[]>(face => GetEmbeddings(face, token));
-        this.buffer = new BufferBlock<Image<Rgb24>>();
-        buffer.LinkTo(transform);
-        this.embeddingsDict = new ConcurrentDictionary<Image<Rgb24>, float[]>();
+        session = new InferenceSession(memoryStream.ToArray(), sessionOptions);
+        embeddingsDict = new ConcurrentDictionary<Image<Rgb24>, float[]>();
     }
 
     ~ClassArcFace()
@@ -37,61 +32,38 @@ public class ClassArcFace
         this.session.Dispose();
     }
 
-    /*
-        Данный способ доступа к API позволяет, передав два изображения, получить результат distance и similarity. Метод startCalculations
-        проверяет, существует ли уже просчитанный embedding для данного изображения, тем самым спасает нас от повторного вычисления
-        embeddings для изображения(но не гарантирует это на 100 процентов). У него есть один большой недостаток:
-        При асинхронном вызове данного метода в консольном приложении, вызов может не успеть добавить в словарь (face, embeddings) новый элемент,
-        тем саммы при параллельной работе нескольких вызовов, метод может повторно просчитать уже существующий embedding
-
-    */
-    public async Task<Tuple<float, float>> startCalculations(Image<Rgb24> face1, Image<Rgb24> face2, CancellationToken token)
+    public async Task CalculateAllEmbeddingsAsync(Image<Rgb24> face, CancellationToken token)
     {
-        float[] embeddings1;
-        float[] embeddings2;
+        await Task.Factory.StartNew(() =>
+        {
+            if (token.IsCancellationRequested)
+                token.ThrowIfCancellationRequested(); // генерируем исключение
+            if (embeddingsDict.ContainsKey(face))
+            {
+                return;
+            }
+            var inputs = new List<NamedOnnxValue> { NamedOnnxValue.CreateFromTensor("data", ImageToTensor(face)) };
+            IDisposableReadOnlyCollection<DisposableNamedOnnxValue> results;
+            lock (session)
+            {
+                results = session.Run(inputs);
+            }
+            if (token.IsCancellationRequested)
+                token.ThrowIfCancellationRequested(); // генерируем исключение
+            float[] embeddings = Normalize(results.First(v => v.Name == "fc1").AsEnumerable<float>().ToArray());
+            embeddingsDict.TryAdd(face, embeddings);
+        }, token, TaskCreationOptions.LongRunning, TaskScheduler.Default);
+    }
 
-        if (embeddingsDict.ContainsKey(face1) == true && embeddingsDict.ContainsKey(face2) == true)
-        {
-            //Console.WriteLine("face1 and face2 are already exists");
-            embeddings1 = embeddingsDict[face1];
-            embeddings2 = embeddingsDict[face2];
-        }
-        else if(embeddingsDict.ContainsKey(face1) == true && embeddingsDict.ContainsKey(face2) == false)
-        {
-            //Console.WriteLine("face1 is already exists");
-            embeddings1 = embeddingsDict[face1];
-            buffer.Post(face2);
-            embeddings2 = await transform.ReceiveAsync();
-            embeddingsDict.TryAdd(face2, embeddings2); //добавляем в словарь вычисленный embedding2
-        }
-        else if (embeddingsDict.ContainsKey(face1) == false && embeddingsDict.ContainsKey(face2) == true)
-        {
-            //Console.WriteLine("face2 is already exists");
-            embeddings2 = embeddingsDict[face2];
-            buffer.Post(face1);
-            embeddings1 = await transform.ReceiveAsync();
-            embeddingsDict.TryAdd(face1, embeddings1); //добавляем в словарь вычисленный embedding1
-        }
-        else
-        {
-            //Console.WriteLine("face1 and face2 are not exists");
-            buffer.Post(face1);
-            buffer.Post(face2);
-            embeddings1 = await transform.ReceiveAsync();
-            embeddings2 = await transform.ReceiveAsync();
-            embeddingsDict.TryAdd(face1, embeddings1); //добавляем в словарь вычисленный embedding1
-            embeddingsDict.TryAdd(face2, embeddings2); //добавляем в словарь вычисленный embedding2
-        }
-
-        //buffer.Complete();
-
+    public async Task<Tuple<float, float>> CalculateDistanceSimilarity(Image<Rgb24> face1, Image<Rgb24> face2)
+    {
+        float[] embeddings1 = embeddingsDict[face1];
+        float[] embeddings2 = embeddingsDict[face2];
         var dist = Distance(embeddings1, embeddings2);
         var similarity = Similarity(embeddings1, embeddings2);
         await dist;
         await similarity;
-
         return Tuple.Create(dist.Result, similarity.Result);
-
     }
 
     async Task<float> Distance(float[] v1, float[] v2)
@@ -142,49 +114,6 @@ public class ClassArcFace
     public void Dispose()
     {
         throw new NotImplementedException();
-    }
-
-    
-    float[] GetEmbeddings(Image<Rgb24> face, CancellationToken token)
-    {
-        if (token.IsCancellationRequested)
-            token.ThrowIfCancellationRequested(); // генерируем исключение
-
-        var inputs = new List<NamedOnnxValue> { NamedOnnxValue.CreateFromTensor("data", ImageToTensor(face)) };
-        using IDisposableReadOnlyCollection<DisposableNamedOnnxValue> results = session.Run(inputs);
-        if (token.IsCancellationRequested)
-            token.ThrowIfCancellationRequested(); // генерируем исключение
-        return Normalize(results.First(v => v.Name == "fc1").AsEnumerable<float>().ToArray());
-    }
-
-    /*
-        Данные два метода дают альтернативный доступ к API. Сначала в консольном приложении мы запускаем метод GalculateAllEmbeddings
-        для вычисления embeddings для каждого изображения.
-        После этого запускаем метод CalculateDistanceSimilarity, что бы посчитать distance и similarity между любыми двумя изображениями.
-        Важным преимуществом данного подхода является то, что embeddings для лиц не будет постоянно пересчитываться. Он сохраняется в словаре
-        и при необходимости выдает значение embeddings по ключу face
-    */
-    public async Task CalculateAllEmbeddings(Image<Rgb24> face, CancellationToken token)  //метод считает все embeddings всех лиц и помещает в словарь
-    {
-        if (token.IsCancellationRequested)
-            token.ThrowIfCancellationRequested(); // генерируем исключение
-        if (embeddingsDict.ContainsKey(face) == false)
-        {
-            buffer.Post(face);
-            float[] embeddings = await transform.ReceiveAsync();
-            embeddingsDict.TryAdd(face, embeddings);
-        }   
-    }
-
-    public async Task<Tuple<float, float>> CalculateDistanceSimilarity(Image<Rgb24> face1, Image<Rgb24> face2)
-    {
-        float[] embeddings1 = embeddingsDict[face1];
-        float[] embeddings2 = embeddingsDict[face2];
-        var dist = Distance(embeddings1, embeddings2);
-        var similarity = Similarity(embeddings1, embeddings2);
-        await dist;
-        await similarity;
-        return Tuple.Create(dist.Result, similarity.Result);
     }
 
 }
--- ClassLibrary/testConsole/Program.cs
+++ ClassLibrary/testConsole/Program.cs
@@ -35,9 +35,8 @@ namespace testConsole
             CancellationTokenSource cancelTokenSource = new CancellationTokenSource();
             CancellationToken token = cancelTokenSource.Token;
 
-            ClassArcFace obj1 = new ClassArcFace(token);
-            ClassArcFace obj2 = new ClassArcFace(token);
-            ClassArcFace obj3 = new ClassArcFace(token);
+            ClassArcFace obj1 = new ClassArcFace();
+            ClassArcFace obj2 = new ClassArcFace();
 
 
             using var face1 = Image.Load<Rgb24>("face1.png");
@@ -46,100 +45,44 @@ namespace testConsole
             using var face3 = Image.Load<Rgb24>("madiyar1.jpg");
             using var face4 = Image.Load<Rgb24>("madiyar2.jpg");
 
-            //синхронный запуск
             Stopwatch sw1 = new Stopwatch();
             sw1.Start();
-            var res1 = obj1.startCalculations(face1, face2, token);
+
+            var emb1 = obj1.CalculateAllEmbeddingsAsync(face1, token);
+            var emb2 = obj1.CalculateAllEmbeddingsAsync(face2, token);
+            var emb3 = obj1.CalculateAllEmbeddingsAsync(face3, token);
+            var emb4 = obj1.CalculateAllEmbeddingsAsync(face4, token);
+            //cancelTokenSource.Cancel();
+            await emb1;
+            await emb2;
+            await emb3;
+            await emb4;
+            var res1 = obj1.CalculateDistanceSimilarity(face1, face2);
+            var res2 = obj1.CalculateDistanceSimilarity(face1, face1);
+            var res3 = obj1.CalculateDistanceSimilarity(face3, face4);
             await res1;
-            var res2 = obj1.startCalculations(face1, face2, token);
             await res2;
-            var res3 = obj1.startCalculations(face1, face2, token);
             await res3;
+
             sw1.Stop();
 
-            Console.WriteLine("synchronous calculations:");
-            Console.WriteLine("------ Results 1 ------");
-            Console.WriteLine($"Distance = {res1.Result.Item1}");
-            Console.WriteLine($"Similarity =  {res1.Result.Item2}");
-            Console.WriteLine("------ Results 2 ------");
-            Console.WriteLine($"Distance = {res2.Result.Item1}");
-            Console.WriteLine($"Similarity =  {res2.Result.Item2}");
-            Console.WriteLine("------ Results 3 ------");
-            Console.WriteLine($"Distance = {res3.Result.Item1}");
-            Console.WriteLine($"Similarity =  {res3.Result.Item2}");
+            Console.WriteLine("------ Results of face1 and face2 ------");
+            Console.WriteLine($"Distance: {res1.Result.Item1}");
+            Console.WriteLine($"Similarity: {res1.Result.Item2}");
+
+            Console.WriteLine("------ Results of face1 and face1 ------");
+            Console.WriteLine($"Distance: {res2.Result.Item1}");
+            Console.WriteLine($"Similarity: {res2.Result.Item2}");
+
+            Console.WriteLine("------ Results of face3 and face4 ------");
+            Console.WriteLine($"Distance: {res3.Result.Item1}");
+            Console.WriteLine($"Similarity: {res3.Result.Item2}");
+
             Console.WriteLine("Time elapsed: {0} ms", sw1.ElapsedMilliseconds);
-            Console.WriteLine("------------------------------");
 
 
-            //асинхронный запуск
-            Stopwatch sw2 = new Stopwatch();
-            sw2.Start();
-            var res4 = obj2.startCalculations(face1, face2, token);
-            //Thread.Sleep(3000);
-            var res5 = obj2.startCalculations(face1, face2, token);
-            var res6 = obj2.startCalculations(face1, face2, token);
-            await res4;
-            await res5;
-            await res6;
-            sw2.Stop();
-
-            Console.WriteLine("asynchronous calculations:");
-            Console.WriteLine("------ Results 1 ------");
-            Console.WriteLine($"Distance = {res4.Result.Item1}");
-            Console.WriteLine($"Similarity =  {res4.Result.Item2}");
-            Console.WriteLine("------ Results 2 ------");
-            Console.WriteLine($"Distance = {res5.Result.Item1}");
-            Console.WriteLine($"Similarity =  {res5.Result.Item2}");
-            Console.WriteLine("------ Results 3 ------");
-            Console.WriteLine($"Distance = {res6.Result.Item1}");
-            Console.WriteLine($"Similarity =  {res6.Result.Item2}");
-            Console.WriteLine("Time elapsed: {0} ms", sw2.ElapsedMilliseconds);
-            Console.WriteLine("------------------------------");
-
-            //Отмена вычислений
-
-            var res7 = obj2.startCalculations(face1, face2, token);
-            //Thread.Sleep(1000);
-            //cancelTokenSource.Cancel();
-            Console.WriteLine("------ Results 1 ------");
-            Console.WriteLine($"Distance = {res7.Result.Item1}");
-            Console.WriteLine($"Similarity =  {res7.Result.Item2}");
-            Console.WriteLine("------------------------------");
+            var emb5 = obj2.CalculateAllEmbeddingsAsync(face1, token);
             
-
-            //Альтернативный доступ к API
-            Stopwatch sw3 = new Stopwatch();
-            sw3.Start();
-            //вычисляем embeddings для каждого лица
-            var t1 = obj3.CalculateAllEmbeddings(face1, token);
-            var t2 = obj3.CalculateAllEmbeddings(face2, token);
-            var t3 = obj3.CalculateAllEmbeddings(face3, token);
-            var t4 = obj3.CalculateAllEmbeddings(face4, token);
-            await t1;
-            await t2;
-            await t3;
-            await t4;
-            //вычисляем distance и similarity между любыми двумя изображениями
-            var res_face1_face2 = obj3.CalculateDistanceSimilarity(face1, face2);
-            var res_face3_face4 = obj3.CalculateDistanceSimilarity(face3, face4);
-            var res_face1_face1 = obj3.CalculateDistanceSimilarity(face1, face1);
-            await res_face1_face2;
-            await res_face3_face4;
-            await res_face1_face1;
-            sw3.Stop();
-            Console.WriteLine("Result for face1 and face2");
-            Console.WriteLine($"Distance = {res_face1_face2.Result.Item1}");
-            Console.WriteLine($"Similarity =  {res_face1_face2.Result.Item2}");
-            Console.WriteLine("------------");
-            Console.WriteLine("Result for face3 and face4");
-            Console.WriteLine($"Distance = {res_face3_face4.Result.Item1}");
-            Console.WriteLine($"Similarity =  {res_face3_face4.Result.Item2}");
-            Console.WriteLine("------------");
-            Console.WriteLine("Result for face1 and face1");
-            Console.WriteLine($"Distance = {res_face1_face1.Result.Item1}");
-            Console.WriteLine($"Similarity =  {res_face1_face1.Result.Item2}");
-            Console.WriteLine("------------");
-            Console.WriteLine("Time elapsed: {0} ms", sw2.ElapsedMilliseconds);
 
             cancelTokenSource.Dispose();
         }
