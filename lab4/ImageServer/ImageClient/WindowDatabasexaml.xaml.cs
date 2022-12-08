d2083181ae1a6f7c28a410f3b267a2ac9455a0e6
madiyar99 <madiyar021@gmail.com>
2022-12-08 17:26:21 +0300

    lab4 v1.0

---
+++ lab4/ImageServer/ImageClient/WindowDatabasexaml.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Text.Json;
+using Polly;
+using Polly.Retry;
+
+namespace ImageClient
+{
+    /// <summary>
+    /// Логика взаимодействия для WindowDatabasexaml.xaml
+    /// </summary>
+    public partial class WindowDatabasexaml : Window
+    {
+        private readonly string url = "http://localhost:5032/api/images";
+        public ObservableCollection<ImageContracts.Image> ImagesCollection { get; private set; }
+        private const int MaxRetries = 3;
+        private readonly AsyncRetryPolicy _retryPolicy;
+        public WindowDatabasexaml()
+        {
+            _retryPolicy = Policy.Handle<HttpRequestException>().WaitAndRetryAsync(MaxRetries, times =>
+                TimeSpan.FromMilliseconds(Math.Exp(times) * 250));
+            ImagesCollection = new ObservableCollection<ImageContracts.Image>();
+            GetAllImages();
+            InitializeComponent();
+            DataContext = this;         
+        }
+
+        public async void GetAllImages()
+        {
+            await _retryPolicy.ExecuteAsync(async () =>
+            {
+                HttpClient client = new HttpClient();
+                var taskGetAllImages = await client.GetAsync(url);
+                var id_list = JsonConvert.DeserializeObject<List<int>>(taskGetAllImages.Content.ReadAsStringAsync().Result);
+
+                for (int i = 0; i < id_list.Count; i++)
+                {
+                    var taskGetImage = await client.GetAsync($"{url}/{id_list[i]}");
+                    var image = JsonConvert.DeserializeObject<ImageContracts.Image>(taskGetImage.Content.ReadAsStringAsync().Result);
+                    ImagesCollection.Add(image);
+                }
+            });              
+        }
+        private async void Button_Delete_Image(object sender, RoutedEventArgs e)
+        {         
+            await _retryPolicy.ExecuteAsync(async () =>
+            {
+                HttpClient client = new HttpClient();
+                var image = ImagesCollection[ImagesCollectionListBox.SelectedIndex];
+                var task = await client.DeleteAsync($"{url}/{image.Id}");
+                var result = JsonConvert.DeserializeObject<int>(task.Content.ReadAsStringAsync().Result);
+                if (result == 1)
+                {
+                    MessageBox.Show("Удаление прошло успешно!");
+                    ImagesCollection.Remove(image);
+                }
+                else
+                {
+                    MessageBox.Show("Удаление завершилось неудачей, пожалуйста, повторите попытку позже!");
+                }
+            });
+        }
+    }
+}
---
+++ lab4/ImageServer/ImageServer/Database/ImagesDatabase.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using Microsoft.EntityFrameworkCore;
+using SixLabors.ImageSharp.PixelFormats;
+using ImageContracts;
+using ClassLibrary;
+namespace ImageServer.Database
+{
+    public class ImagesContext : DbContext
+    {
+        public DbSet<Image> Images { get; set; }
+        public DbSet<ImageDetails> Details { get; set; }
+
+        public ImagesContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder o)
+        {
+            o.UseSqlite("Data Source=images.db");
+        }
+    }
+    public interface IImagesDb
+    {
+        Task<List<int>> GetAllImages();
+        Task<int> PostImage(byte[] img, string path, CancellationToken token);
+        Task<Image> TryGetImageById(int id);
+        Task<int> TryDeleteImage(int id);
+    }
+    public class ImagesDatabase : IImagesDb
+    {
+        ClassArcFace obj1 = new ClassArcFace();
+        public async Task<List<int>> GetAllImages()
+        {
+            var result = new List<int>();
+            try
+            {
+                using (var db = new ImagesContext())
+                {
+                    var query = db.Images;
+                    foreach (var image in query)
+                    {
+                        result.Add(image.Id);
+                    }
+                    return result;
+                }
+            }
+            catch (Exception ex)
+            {
+                return result;
+            }
+        }
+
+        public async Task<Image> TryGetImageById(int id)
+        {
+            try
+            {
+                using (var db = new ImagesContext())
+                {
+                    Image image = db.Images.Where(x => x.Id == id).Include(x => x.Details).FirstOrDefault();
+                    return image; 
+                }
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        public async Task<int> TryDeleteImage(int id)
+        {
+            try
+            {
+                using (var db = new ImagesContext())
+                {
+                    var deletedImage = db.Images.Where(x => x.Id == id).Include(x => x.Details).First();
+                    if (deletedImage == null)
+                    {
+                        return -1;
+                    }
+                    db.Details.Remove(deletedImage.Details);
+                    db.Images.Remove(deletedImage);
+                    db.SaveChanges();
+                }
+                return 1;
+            }
+            catch (Exception ex)
+            {
+                return -1;
+            }
+        }
+
+        public async Task<int> PostImage(byte[] image, string path, CancellationToken token)
+        {
+            try
+            {
+                Image existImage = null;
+                using (var db = new ImagesContext())   //По хэш-коду ищем изображение, если хэш-код совпадает, то дальше сверяем содержмое 
+                {
+                    string hash = Image.GetHash(image);
+                    var q = db.Images.Where(x => x.Hash == hash)
+                        .Include(x => x.Details)
+                        .Where(x => Equals(x.Details.Data, image));
+                    if (q.Any())
+                    {
+                        existImage = q.First();
+                    }
+                }
+                if (existImage is not null)   //Если в базе данных уже есть данное изображение, то просто возвращаем его id
+                {
+                    return existImage.Id;
+                }
+                else                        //Иначе вычисляем embeddings для данного изображения, добавялем в базу данных и возвращаем его id
+                {
+                    var face = SixLabors.ImageSharp.Image.Load<Rgb24>(path);
+                    var task1 = obj1.CalculateAllEmbeddingsAsync(face, token);
+                    await task1;
+                    Console.WriteLine("Step1");
+                    using (var db = new ImagesContext())           
+                    {
+                        var newImageDetails = new ImageDetails { Data = image };
+                        var byteArray = new byte[task1.Result.Length * 4];
+                        Buffer.BlockCopy(task1.Result, 0, byteArray, 0, byteArray.Length);
+                        Image newImage = new Image
+                        {
+                            Name = path,
+                            Embedding = byteArray,
+                            Details = newImageDetails,
+                            Hash = Image.GetHash(image)
+                        };
+                        db.Add(newImage);
+                        db.SaveChanges();
+                    }
+
+                    existImage = null;
+                    using (var db = new ImagesContext()) 
+                    {
+                        string hash = Image.GetHash(image);
+                        var q = db.Images.Where(x => x.Hash == hash)
+                            .Include(x => x.Details)
+                            .Where(x => Equals(x.Details.Data, image));
+                        if (q.Any())
+                        {
+                            existImage = q.First();
+                        }              
+                    }
+                    if (existImage is not null)   //Если в базе данных уже есть данное изображение, то просто возвращаем его id
+                    {
+                        return existImage.Id;
+                    }
+                    else
+                    {
+                        return -1;
+                    }
+                }
+            }
+            catch (OperationCanceledException e1)
+            {
+                return -1;
+            }
+        }
+    }
+}
